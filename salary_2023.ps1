f52248b7ae83e26ba2569e2a96eaf1933bb5906a
akosijesyang <84263753+akosijesyang@users.noreply.github.com>
2023-09-28 19:43:00 +0800

    Add files via upload

---
+++ salary_2023.ps1
@@ -0,0 +1,8 @@
+﻿# Create a new HTML file
+New-Item -Path .\salary_2023.html -ItemType File
+
+# Write the hello world message to the HTML file
+Set-Content -Path .\salary_2023.html -Value "<html><body><h1>You have been pwned!!</h1></body></html>"
+
+# Launch the HTML file in the default browser
+Start-Process -FilePath .\salary_2023.html
